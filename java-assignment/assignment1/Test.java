1ebe244ea93906b2f0d9d864336fbd0ac2e399de
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2019-08-04 11:27:48 +0530

    Update Test.java

--- java-assignment/assignment1/Test.java
+++ java-assignment/assignment1/Test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Test {
+class Test {
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
         int score[]=new int[5];
